32480b243d709459d72b1da99227a0dbc77069e9
rellimylime <ermiller@ucsb.edu>
2025-08-28 12:05:24 -0700

    self-assessment.md added

---
+++ scripts/0_init_env.R
@@ -0,0 +1,15 @@
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+##                                                                            --
+## ------------------------ ENVIRONMENT INITIALIZATION---------------------------
+##                                                                            --
+## ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Vector of required packages
+package_list <- c("tidyverse", "here", "lubridate")
+
+# Check if package is installed and load
+for (package in package_list) {
+  if (!requireNamespace(package, quietly = TRUE)) {
+    install.packages(package)
+  }
+}
